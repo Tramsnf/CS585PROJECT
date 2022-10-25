c67bd456c093b67a2482fff48a8802cd4cec7bdd
Oluwaseyi <oi-adelusi@wiu.edu>
2022-10-25 16:25:33 -0500

    CommitNow

---
+++ security/studentsportal/src/main/java/org/portal/students/studentsportal/configs/RegularJsonConfigProviderFactory.java
@@ -0,0 +1,5 @@
+package org.portal.students.studentsportal.configs;
+
+import org.keycloak.services.util.JsonConfigProviderFactory;
+
+public class RegularJsonConfigProviderFactory extends JsonConfigProviderFactory { }
