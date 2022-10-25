af6effa8fcc1bfc4e056bdb54eeade0f7218e904
Edgar-23 <66624791+Edgar-23@users.noreply.github.com>
2022-10-25 15:16:22 -0500

    StudentPortal

---
+++ security/studentsportal/src/test/java/org/portal/students/studentsportal/StudentsportalApplicationTests.java
@@ -0,0 +1,13 @@
+//package org.portal.students.studentsportal;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class StudentsportalApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
