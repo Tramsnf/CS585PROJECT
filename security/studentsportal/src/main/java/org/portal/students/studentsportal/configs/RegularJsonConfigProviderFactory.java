c6eac1ca9616dfcbc08589f8b5469ccb889a3ccb
Tramsnf <96598884+Tramsnf@users.noreply.github.com>
2022-12-02 20:38:05 -0600

    delection took place on some zombie files

--- security/studentsportal/src/main/java/org/portal/students/studentsportal/configs/RegularJsonConfigProviderFactory.java
+++
@@ -1,5 +0,0 @@
-package org.portal.students.studentsportal.configs;
-
-import org.keycloak.services.util.JsonConfigProviderFactory;
-
-public class RegularJsonConfigProviderFactory extends JsonConfigProviderFactory { }
