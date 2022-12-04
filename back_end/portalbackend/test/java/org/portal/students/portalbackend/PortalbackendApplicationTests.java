8fb13bc07c7277954aec0588f0f9dbfb87f66d08
Oluwaseyi <oi-adelusi@wiu.edu>
2022-12-04 17:01:19 -0600

    Create PortalbackendApplicationTests.java

---
+++ back_end/portalbackend/test/java/org/portal/students/portalbackend/PortalbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.portal.students.portalbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortalbackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
